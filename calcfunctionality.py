d6073132e20cfe73de6124ec13f2277a61fe291e
vanderst <71615636+vanderst@users.noreply.github.com>
2021-11-10 19:05:55 -0700

    Add files via upload

--- calcfunctionality.py
+++ calcfunctionality.py
@@ -1,4 +1,5 @@
 from PyQt5 import QtWidgets, uic, QtGui
+import subprocess
 import csv
 
 app = QtWidgets.QApplication([])
@@ -18,73 +19,75 @@ def backtoprofile():                                    #Back button functionali
     mainmenu.show()
 
 def calculateprofit():                                  #Calculation functionality
-    row1 = int(calcform.quantity.text()) * int(calcform.price.text())
-    row2 = int(calcform.quantity_2.text()) * int(calcform.price_2.text())
-    row3 = int(calcform.quantity_3.text()) * int(calcform.price_3.text())
-    row4 = int(calcform.quantity_4.text()) * int(calcform.price_4.text())
-    row5 = int(calcform.quantity_5.text()) * int(calcform.price_5.text())
-    row6 = int(calcform.quantity_6.text()) * int(calcform.price_6.text())
-    row7 = int(calcform.quantity_7.text()) * int(calcform.price_7.text())
-    row8 = int(calcform.quantity_8.text()) * int(calcform.price_8.text())
-    row9 = int(calcform.quantity_9.text()) * int(calcform.price_9.text())
-    row10 = int(calcform.quantity_10.text()) * int(calcform.price_10.text())
+    row1 = float(calcform.quantity.text()) * float(calcform.price.text())
+    row2 = float(calcform.quantity_2.text()) * float(calcform.price_2.text())
+    row3 = float(calcform.quantity_3.text()) * float(calcform.price_3.text())
+    row4 = float(calcform.quantity_4.text()) * float(calcform.price_4.text())
+    row5 = float(calcform.quantity_5.text()) * float(calcform.price_5.text())
+    row6 = float(calcform.quantity_6.text()) * float(calcform.price_6.text())
+    row7 = float(calcform.quantity_7.text()) * float(calcform.price_7.text())
+    row8 = float(calcform.quantity_8.text()) * float(calcform.price_8.text())
+    row9 = float(calcform.quantity_9.text()) * float(calcform.price_9.text())
+    row10 = float(calcform.quantity_10.text()) * float(calcform.price_10.text())
 
-    prodprice = int(calcform.productprice.text())
-    result = str(prodprice-(row1 + row2 + row3 + row4 + row5 + row6 + row7 + row8 + row9 + row10))
+    prodprice = float(calcform.productprice.text())
+    result = "$" + str(prodprice-(row1 + row2 + row3 + row4 + row5 + row6 + row7 + row8 + row9 + row10))
     calcform.label_12.setText(result)
 
+def populateFields(component_name, price_name):
 
-def componententer(comp_number):                #Text box call functionality
-
-    with open('dummydata.csv', 'rt')as f:
+    with open('itemPrices.csv', 'r')as f:
         data = csv.reader(f)
         entries = []
         for row in data:
             entries.append(row)
-        product = entries[0][0]
-        price = entries[0][1]
+        product = entries[1][0]
+        price = entries[1][1].lstrip('$')
+    component_name.setText(str(product))
+    price_name.setText(str(price))
+    f.close()
+
+def writeLinkToRow(component_name):
+
+    with open('itemPrices.csv', 'w', newline='')as f:
+        writer = csv.writer(f)
+        writer.writerows([["items", "prices", "url"],["","",component_name.text()]])
+    f.close()
+
+    subprocess.run(['python','priceScraper.py'])
+
+def componententer(comp_number):                #Text box call functionality
 
     if comp_number == 1:
-        calcform.price.setText(str(price))
-        calcform.component.setText(str(product))
+        writeLinkToRow(calcform.component)
+        populateFields(calcform.component, calcform.price)
 
     elif comp_number == 2:
-        calcform.price_2.setText(str(price))
-        calcform.component_2.setText(str(product))
+        writeLinkToRow(calcform.component_2)
+        populateFields(calcform.component_2, calcform.price_2)
 
     elif comp_number == 3:
-        calcform.price_3.setText(str(price))
-        calcform.component_3.setText(str(product))
+        writeLinkToRow(calcform.component_3)
+        populateFields(calcform.component_3, calcform.price_3)
 
-    elif comp_number == 4:
-        calcform.price_4.setText(str(price))
-        calcform.component_4.setText(str(product))
+    # elif comp_number == 4:
+    #
+    # elif comp_number == 5:
+    #
+    # elif comp_number == 6:
+    #
+    # elif comp_number == 7:
+    #
+    # elif comp_number == 8:
+    #
+    # elif comp_number == 9:
+    #
+    # elif comp_number == 10:
 
-    elif comp_number == 5:
-        calcform.price_5.setText(str(price))
-        calcform.component_5.setText(str(product))
 
-    elif comp_number == 6:
-        calcform.price_6.setText(str(price))
-        calcform.component_6.setText(str(product))
-
-    elif comp_number == 7:
-        calcform.price_7.setText(str(price))
-        calcform.component_7.setText(str(product))
-
-    elif comp_number == 8:
-        calcform.price_8.setText(str(price))
-        calcform.component_8.setText(str(product))
-
-    elif comp_number == 9:
-        calcform.price_10.setText(str(price))
-        calcform.component_9.setText(str(product))
-
-    elif comp_number == 10:
-        calcform.price_9.setText(str(price))
-        calcform.component_10.setText(str(product))
     calcform.exec()
     calcform.show()
+
 
 
 # Button Actions
@@ -117,5 +120,7 @@ calcform.pushButton.clicked.connect(backtoprofile)
 
 mainmenu.show()
 app.exec_()
+
+
 
 
